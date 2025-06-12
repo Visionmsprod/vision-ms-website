05334a6704a8a9bee02ecf71cb3a47183ba7f74f
Visionmsprod <luiscarlosmueses@gmail.com>
2025-06-12 18:17:45 -0500

    Add files via upload

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/logo1.png");
+  eleventyConfig.addPassthroughCopy("src/luisk.jpg");
+  eleventyConfig.addPassthroughCopy("src/favicon.png");
+  eleventyConfig.addPassthroughCopy({"src/_redirects": "_redirects"});
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    templateFormats: ["njk", "md", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
