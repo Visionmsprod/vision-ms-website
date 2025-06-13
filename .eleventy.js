6a2020eed53243b02cea168cbf5204d045330975
Visionmsprod <luiscarlosmueses@gmail.com>
2025-06-13 03:03:28 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,36 +1,45 @@
 module.exports = function(eleventyConfig) {
-eleventyConfig.addPassthroughCopy("src/css");
-eleventyConfig.addPassthroughCopy("src/js");
-eleventyConfig.addPassthroughCopy("src/images");
-eleventyConfig.addPassthroughCopy("src/admin");
-eleventyConfig.addPassthroughCopy({"src/_redirects": "_redirects"});
-eleventyConfig.addPassthroughCopy("src/logo1.png");
-eleventyConfig.addPassthroughCopy("src/luisk.jpg");
-eleventyConfig.addPassthroughCopy("src/favicon.png");
-return {
-dir: {
-input: "src",
-includes: "_includes",
-data: "_data",
-output: "_site"
-},
-htmlTemplateEngine: "njk"
-};
-};
-</details>
+  
+  // --- FILTRO PERSONALIZADO PARA EL COTIZADOR ---
+  eleventyConfig.addFilter("filterby", (collection, key, value) => {
+    if (!collection) {
+      return [];
+    }
+    return collection.filter(item => {
+        const data = item.data;
+        if(data && data[key] !== undefined) {
+            return data[key] === value;
+        }
+        return false;
+    });
+  });
 
-<details>
-<summary><strong>2. Código Final para `package.json`</strong></summary>
-```json
-{
-  "name": "vision-ms-website",
-  "version": "1.0.0",
-  "description": "Sitio web para Visión MS Producción Audiovisual",
-  "scripts": {
-    "start": "npx @11ty/eleventy --serve",
-    "build": "npx @11ty/eleventy"
-  },
-  "devDependencies": {
-    "@11ty/eleventy": "^2.0.1"
-  }
-}
+  // --- COLECCIÓN PERSONALIZADA PARA EL PORTAFOLIO ---
+  eleventyConfig.addCollection("portafolio", function(collectionApi) {
+    return collectionApi.getFilteredByTag("portafolio").sort((a, b) => {
+      return b.date - a.date;
+    });
+  });
+  
+  // --- COPIAR ARCHIVOS ESTÁTICOS ---
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy({"src/_redirects": "_redirects"});
+  eleventyConfig.addPassthroughCopy("src/logo1.png");
+  eleventyConfig.addPassthroughCopy("src/luisk.jpg");
+  eleventyConfig.addPassthroughCopy("src/favicon.png");
+
+  // --- CONFIGURACIÓN DE ELEVENTY ---
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
